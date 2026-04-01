ed19922c7e12db985edbfb2eb9dee87f511bbba5
vishwajeetdhumalwork3005 <vishwajeetdhumalwork2026@gmail.com>
2026-04-01 15:36:45 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char c: s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }else if(st.empty()){
+                return false;
+            }else if(st.top()=='(' && c==')' || 
+                     st.top()=='{' && c=='}' || 
+                     st.top()=='[' && c==']'){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+        return st.empty();
+    }
+};
